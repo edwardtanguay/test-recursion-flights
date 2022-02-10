172907e891fcffb288cf6766cbcc59bfc736e5c9
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-02-10 10:38:28 +0100

    before change to two

--- test.js
+++ test.js
@@ -39,21 +39,21 @@ const commonDestinations = [];
 const compareFlights = (currentIndex) => {
 	const flights1 = flightCollections[currentIndex];
 	const flights2 = currentIndex === flightCollections.length - 1 ? null : flightCollections[currentIndex + 1];
-	flights1.forEach((flight1) => {
-		if (flights2 !== null) {
+	if (flights2 !== null) {
+		flights1.forEach((flight1) => {
 			flights2.forEach(flight2 => {
 				if (flight1.airport === flight2.airport) {
 					compareFlights(currentIndex + 1);
 				}
 			});
-		} else {
-			flights2.forEach(flight2 => {
-				if (flight1.airport === flight2.airport) {
-					commonDestinations.push(flight.airport);
-				}
-			});
-		}
-	});
+		});
+	} else {
+		flights2.forEach(flight2 => {
+			if (flight1.airport === flight2.airport) {
+				commonDestinations.push(flight.airport);
+			}
+		});
+	}
 }
 
 compareFlights(0);
